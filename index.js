119e88c1ba1827e3902dc10649d858d661e9fee4
MaryKaheti <kahetikihara@gmail.com>
2023-03-16 14:40:53 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide () {
+    let num1 = 2000;
+    let num2 = 100;
+    return num1/num2
+}
+console.log(divide())
+const square = (parameter1) => parameter1 * parameter1;
+square(2,2)
+
+const add = (bok1, bok2) => bok1 + bok2;
+add(5,6)
